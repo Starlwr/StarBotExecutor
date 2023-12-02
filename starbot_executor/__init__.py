d5b921ec928e99eb8410d615eaa374d6d0acad4b
LWR <lwr1104@qq.com>
2023-12-03 00:49:05 +0800

    feat: Set WindowsSelectorEventLoopPolicy in Windows system

--- starbot_executor/__init__.py
+++ starbot_executor/__init__.py
@@ -1,1 +1,7 @@
+import asyncio
+import platform
+
 from .core.executor import executor
+
+if 'windows' in platform.system().lower():
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
